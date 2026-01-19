9bb5bdd1cff62572cc1d16e1706a7b1686ba07fa
h1divp <71522316+h1divp@users.noreply.github.com>
2026-01-19 16:33:25 -0500

    chore: update

---
+++ .scripts/screenshot-fullscreen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SAVEDIR=~/Media/Screenshots
+FILENAME="$SAVEDIR/$(date +'%Y-%m-%d-%H%M%S_screenshot.png')"
+EXPENDED_FILENAME="${FILENAME/#\~/$HOME}"
+ 
+grim "$EXPENDED_FILENAME"
+# swappy -f "$EXPENDED_FILENAME" -o "$EXPENDED_FILENAME"
+ 
+wl-copy < "$EXPENDED_FILENAME"
+notify-send "Screenshot" "File saved and copied to the clipboard."
