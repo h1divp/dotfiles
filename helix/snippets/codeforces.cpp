787edca0daaf799af8167c220cd18086a4d6ac27
h1divp <71522316+h1divp@users.noreply.github.com>
2026-05-20 14:42:57 -0400

    feat: add helix config

---
+++ helix/snippets/codeforces.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+#define f(i,start,n) for (int i=start;i<n;i++)
+#define all(a) (a).begin(), (a).end()
+void printarr(vi v) {
+    f(i,0,v.size()) {
+        cout << i << "=" << v[i] << " ";
+    }
+    cout << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    solve();
+}
